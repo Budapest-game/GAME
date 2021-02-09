cd1f25324aefae531cc5ad1ecdad9f290f1f7ce2
Praktikumovsky <>
2021-02-09 22:33:54 +0300

    Форма регистрации начало

--- src/pages/registration/registration.tsx
+++ src/pages/registration/registration.tsx
@@ -1,11 +1,49 @@
 import React, { PureComponent } from 'react';
-
+import './form.css';
 export class Registration extends PureComponent {
+  signup(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-            <div>
-                <h1>Регистрация</h1>
-            </div>
+      <form onSubmit={this.signup}>
+        <h1>Регистрация</h1>
+        <label>
+          <span>Почта</span>
+          <input type="text" placeholder="Почта"/>
+        </label>
+        <label>
+          <span>Логин</span>
+          <input type="text" placeholder="Почта"/>
+        </label>
+        <label>
+          <span>Имя</span>
+          <input type="text" placeholder="Почта"/>
+        </label>
+        <label>
+          <span>Фамилия</span>
+          <input type="text" placeholder="Почта"/>
+        </label>
+        <label>
+          <span>Телефон</span>
+          <input type="text" placeholder="Почта"/>
+        </label>
+        <label>
+          <span>Пароль</span>
+          <input type="text" placeholder="Почта"/>
+        </label>
+        <label>
+          <span>Подтвердите пароль</span>
+          <input type="text" placeholder="Почта"/>
+        </label>
+        <div>
+          <button type="submit">Зарегистрироваться</button>
+        </div>
+        <p>
+          <a href="/login">Войти</a>
+       </p>
+      </form>
     );
   }
 }
