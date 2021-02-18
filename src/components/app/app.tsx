6f0378552c2691003dc93cde678abd5cc0adafb5
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-02-18 22:22:43 +0300

    Перенести обертку ошибок в index.tsx.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -11,51 +11,48 @@ import { Profile } from '../../pages/profile/profile';
 import { Page404 } from '../../pages/page404/page404';
 import { Page500 } from '../../pages/page500/page500';
 import { ControlsDemoPage } from '../../pages/controlsDemoPage/controlsDemoPage';
-import { ErrorBoundary } from '../errorBoundary/errorBoundary';
 
 export default class App extends PureComponent {
   render() {
     return (
-      <ErrorBoundary>
-      <div className="app">
-        <Router>
+    <div className="app">
+      <Router>
+        <div>
+          <Navigation />
           <div>
-            <Navigation />
-            <div>
-              <Switch>
-              <Route path="/authorization">
-                  <Authorization />
-                </Route>
-                <Route path="/registration">
-                  <Registration />
-                </Route>
-                <Route path="/game">
-                  <Game />
-                </Route>
-                <Route path="/leaderboard">
-                  <Leaderboard />
-                </Route>
-                <Route path="/forum">
-                  <Forum />
-                </Route>
-                <Route path="/profile">
-                  <Profile />
-                </Route>
-                <Route path="/page404">
-                  <Page404 />
-                </Route>
-                <Route path="/page500">
-                  <Page500 />
-                </Route>
-                <Route path="/controlsDemoPage">
-                  <ControlsDemoPage />
-                </Route>
-              </Switch>
-            </div>
+            <Switch>
+            <Route path="/authorization">
+                <Authorization />
+              </Route>
+              <Route path="/registration">
+                <Registration />
+              </Route>
+              <Route path="/game">
+                <Game />
+              </Route>
+              <Route path="/leaderboard">
+                <Leaderboard />
+              </Route>
+              <Route path="/forum">
+                <Forum />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+              <Route path="/page404">
+                <Page404 />
+              </Route>
+              <Route path="/page500">
+                <Page500 />
+              </Route>
+              <Route path="/controlsDemoPage">
+                <ControlsDemoPage />
+              </Route>
+            </Switch>
           </div>
-        </Router>
-      </div>
-      </ErrorBoundary>
+        </div>
+      </Router>
+    </div>
     );
   }
 }
