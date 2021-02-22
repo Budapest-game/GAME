468b1e74c4e93122b56e9bf22f4a115cf4457d7d
Praktikumovsky <>
2021-02-22 15:41:54 +0300

    Страница форума

---
+++ src/components/forum/theme/theme.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { cn } from '@bem-react/classname';
+import numbersEnds from '../../../utils/numbersEnds';
+import './theme.css';
+
+export interface ThemeInfo {
+  name: string,
+  postsCount: number,
+}
+export function Theme(info:ThemeInfo): JSX.Element {
+  const Cls = cn('theme');
+  const endCases = ['Ответ', 'Ответа', 'Ответов'];
+  return <div className={Cls()}>
+    <div className={Cls('name')}>{info.name}</div>
+    <div className={Cls('postsCount')}>{info.postsCount} <br/>{numbersEnds(info.postsCount, endCases)}</div>
+  </div>;
+}
