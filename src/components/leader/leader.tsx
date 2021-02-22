0a2580f5091294ea593535014482bdf842f038f9
Praktikumovsky <74737765+Praktikumovsky@users.noreply.github.com>
2021-02-22 15:44:08 +0300

    S5T17: Страница лидерборда (#12)
    * Лидерборд

---
+++ src/components/leader/leader.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { cn } from '@bem-react/classname';
+import './leader.css';
+
+interface LeaderInfo {
+  name: string,
+  score: number,
+  position: number,
+  avatar: string
+}
+
+export function leader(info: LeaderInfo): JSX.Element {
+  const Cls = cn('leader');
+  return <div className={Cls()} key={info.position}>
+      <div className={Cls('position')}>{info.position}</div>
+      <div className={Cls('media')}>
+          <img className={Cls('avatar')} src={info.avatar}/>
+      </div>
+      <div className={Cls('nickname')}>
+          <span>{info.name}</span>
+      </div>
+      <div className={Cls('score')}>
+        <span>{info.score}</span>
+      </div>
+  </div>;
+}
