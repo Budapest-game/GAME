48f0a7f3780dd8a953b096a55ae5261ea9cb1c73
Elena <miss.kolesnik@ya.ru>
2021-03-01 17:27:23 +0300

    S6T36: Добавить Redux-хранилище. (#19)
    * Удалены лишние файлы app.
    
    * Добавлено хранилище и подключено к странице форума.
    
    * Добавлен Redux в registration page.
    
    * Добавить payload.
    
    * Объединить thunk'и c обычными экшенами.
    
    * Удалить mapDispatch.
    
    * Исправить profile page import.
    
    Co-authored-by: Elena Kolesnik <miss.kolesnik@yandex.ru>

--- src/components/forum-theme/forum-theme.tsx
+++ src/components/forum-theme/forum-theme.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { cn } from '@bem-react/classname';
+import { ForumThemeInfo } from '../../store/actionCreators/forum';
 import numbersEnds from '../../utils/numbersEnds';
-import './theme.css';
+import './forum-theme.css';
 
-export interface ThemeInfo {
-  name: string,
-  postsCount: number,
-}
-const Cls = cn('theme');
-const endCases = ['Ответ', 'Ответа', 'Ответов'];
-
-export function Theme(info:ThemeInfo): JSX.Element {
+export function ForumTheme(info: ForumThemeInfo): JSX.Element {
+  const Cls = cn('theme');
+  const endCases = ['Ответ', 'Ответа', 'Ответов'];
   return <div className={Cls()}>
     <div className={Cls('name')}>{info.name}</div>
     <div className={Cls('postsCount')}>{info.postsCount} <br/>{numbersEnds(info.postsCount, endCases)}</div>
