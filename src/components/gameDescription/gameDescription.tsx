e6f47777625055711bd6c7451879c96ec7778f36
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-03-03 21:49:42 +0300

    Вынести текстовый контект в отдельные переменные.

--- src/components/gameDescription/gameDescription.tsx
+++ src/components/gameDescription/gameDescription.tsx
@@ -2,30 +2,24 @@ import React, { PureComponent } from 'react';
 import { cn } from '@bem-react/classname';
 import './gameDescription.css';
 import game from './game.png';
+import textContent from './textContent';
 
 export class GameDescription extends PureComponent {
   render() {
     const ClsGameDescription = cn('gameDescription');
     return (
       <>
-      <h1>Игра &quot;Три в ряд&quot;.</h1>
+      <h1>{textContent.header}</h1>
       <div className={ClsGameDescription()}>
         <div className={ClsGameDescription('text')}>
           <p>
-            В игре требуется собрать 3 и более одинаковых элемента в ряд.
+            {textContent.descriptionStart}
           </p>
           <p>
-            Игровое поле размером N на N элементов.
-            Условие завершения игры - окончание времени (t секунд) или n ходов (перестановок).
-            Можно менять местами элементы. Если в процессе смены местоположения не получается
-            три и более элемента в ряд, действие отменяется и элементы встают на прежнее место.
-            Менять местами можно только элементы находящиеся рядом друг с другом по горизонтали
-            или по вертикали.
-            Взамен убывших элементов сверху в рандомном порядке появляются новые элементы.
+            {textContent.description}
           </p>
           <p>
-            Результат пользователя - это количество &quot;уничтоженных&quot; элементов за время игры
-            или n ходов.
+            {textContent.descriptionEnd}
           </p>
         </div>
         <img className={ClsGameDescription('img')} src={game} />
