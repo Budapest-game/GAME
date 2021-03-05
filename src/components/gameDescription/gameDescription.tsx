3f72bda5d66729de85075c5af7a8921a0edcf29f
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-03-05 19:56:49 +0300

    Ревью-правки.

--- src/components/gameDescription/gameDescription.tsx
+++ src/components/gameDescription/gameDescription.tsx
@@ -1,30 +1,28 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { cn } from '@bem-react/classname';
 import './gameDescription.css';
 import game from './game.png';
 import textContent from './textContent';
 
-export class GameDescription extends PureComponent {
-  render() {
-    const ClsGameDescription = cn('gameDescription');
-    return (
-      <>
-      <h1>{textContent.header}</h1>
-      <div className={ClsGameDescription()}>
-        <div className={ClsGameDescription('text')}>
-          <p>
-            {textContent.descriptionStart}
-          </p>
-          <p>
-            {textContent.description}
-          </p>
-          <p>
-            {textContent.descriptionEnd}
-          </p>
-        </div>
-        <img className={ClsGameDescription('img')} src={game} />
+export function GameDescription(): JSX.Element {
+  const Cls = cn('gameDescription');
+  return (
+    <>
+    <h1>{textContent.header}</h1>
+    <div className={Cls()}>
+      <div className={Cls('text')}>
+        <p>
+          {textContent.descriptionStart}
+        </p>
+        <p>
+          {textContent.description}
+        </p>
+        <p>
+          {textContent.descriptionEnd}
+        </p>
       </div>
-    </>
-    );
-  }
+      <img className={Cls('img')} src={game} />
+    </div>
+  </>
+  );
 }
