e215664898480950ff0ee13fb3a91edacbd2e84e
Elena <miss.kolesnik@ya.ru>
2021-03-08 17:47:11 +0300

    S6T37: Добавить Redux Thunk на страницу лидерборда. (#26)
    * Добавить Redux Thunk на страницу лидерборда.
    
    * Ревью фиксы.
    
    Co-authored-by: Elena Kolesnik <miss.kolesnik@yandex.ru>

--- src/components/leader/leader.tsx
+++ src/components/leader/leader.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { cn } from '@bem-react/classname';
+import { LeaderInfo } from '../../store/actionCreators/leaderboard';
 import './leader.css';
 
+<<<<<<< HEAD
 interface LeaderInfo {
   name: string,
   score: number,
@@ -12,13 +14,17 @@ interface LeaderInfo {
 const Cls = cn('leader');
 
 export function leader(info: LeaderInfo): JSX.Element {
+=======
+export function Leader(info: LeaderInfo): JSX.Element {
+  const Cls = cn('leader');
+>>>>>>> S6T37: Добавить Redux Thunk на страницу лидерборда. (#26)
   return <div className={Cls()} key={info.position}>
       <div className={Cls('position')}>{info.position}</div>
       <div className={Cls('media')}>
-          <img className={Cls('avatar')} src={info.avatar}/>
+        <img className={Cls('avatar')} src={info.avatar}/>
       </div>
       <div className={Cls('nickname')}>
-          <span>{info.name}</span>
+        <span>{info.name}</span>
       </div>
       <div className={Cls('score')}>
           <span>{info.score}</span>
