7b7e09a3648eb4c3c6dcac8a59c66a1e3228af03
Praktikumovsky <>
2021-03-20 00:35:26 +0300

    + проверка

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,13 @@
 import express, { Express } from 'express';
+import https from 'https';
+import path from 'path';
+import fs from 'fs';
 import webpack from 'webpack';
 import devMiddleware from 'webpack-dev-middleware';
 import hotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack/webpack.client';
 import render from './middlewares/render/render';
+import authChecker from './middlewares/authChecker';
 import router from './router';
 
 const app: Express = express();
@@ -14,8 +18,12 @@ const instance = devMiddleware(compiler, { publicPath: '/static/' });
 app.use(instance);
 app.use(hotMiddleware(compiler, { path: '/__webpack_hmr' }));
 app.use(render);
+app.use(authChecker);
 app.use(router);
 
-app.listen(port, () => {
+https.createServer({
+  key: fs.readFileSync(path.join(__dirname, '../../certificates/server.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../../certificates/server.cert')),
+}, app).listen(port, () => {
   console.log(`Сервер запущен, порт: ${port}`);
 });
