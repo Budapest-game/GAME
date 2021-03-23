11963f9e65cf880f88da902bcc614296c0ebca37
Praktikumovsky <74737765+Praktikumovsky@users.noreply.github.com>
2021-03-23 21:59:56 +0300

    S7T66: Добавить API для лидерборда. (#35)

---
+++ src/api/leaderboard/leaderboard.ts
@@ -0,0 +1,23 @@
+import ApiBase from '../base';
+import { Leader } from '../types';
+import { RATING_FIELD_NAME } from '../constants';
+import responseParser from '../responseParser';
+
+interface WrappedInfo{
+  data: Leader
+}
+
+class Leaderboard {
+  public async addLeader(data: Leader) {
+    const resp = await ApiBase.post('/leaderboard', { data, ratingFieldName: RATING_FIELD_NAME });
+    const body = await responseParser<Record<string, string>>(resp);
+    return body;
+  }
+
+  public async get() {
+    const resp = await ApiBase.post('/leaderboard/all', { ratingFieldName: RATING_FIELD_NAME, cursor: 0, limit: 10 });
+    const body = await responseParser<WrappedInfo[]>(resp);
+    return body;
+  }
+}
+export default new Leaderboard();
