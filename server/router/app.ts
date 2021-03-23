5defbcb9c6d1a7d8c03d89b7e2305caa88554223
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-03-23 15:10:57 +0300

    S7T59: Настроить SSR. (#33)
    * Роуты для 404 и 500 страниц.
    
    * Починено клиентское хранилище.
    
    * Пересоздание клиентского хранилища из серверного.

--- server/router/app.ts
+++ server/router/app.ts
@@ -1,12 +1,37 @@
-import { Request, Response, Router } from 'express';
+import {
+  NextFunction, Request, Response, Router,
+} from 'express';
 
-const routes = ['/', '/authorization', '/registration', '/game',
-  '/forum', '/leaderboard', '/profile', '/change-password',
+const routes = [
+  '/',
+  '/authorization',
+  '/registration',
+  '/game',
+  '/forum',
+  '/leaderboard',
+  '/profile',
+  '/change-password',
   '/change-data',
   '/controls-demo',
+  '/page404',
+  '/page500',
 ];
-export function appRoutes(router: Router):void {
+
+export function appRoutes(router: Router): void {
   router.get(routes, (req: Request, res: Response) => {
     res.renderBundle();
+  });
+}
+
+export function notFoundRoute(router: Router): void {
+  router.get('*', (req: Request, res: Response) => {
+    res.redirect('/page404');
+  });
+}
+
+export function errorHandlerRoute(router: Router): void {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+  router.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    res.redirect('/page500');
   });
 }
--- server/router/index.ts
+++ server/router/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { appRoutes } from './app';
+import { appRoutes, notFoundRoute, errorHandlerRoute } from './app';
 import { staticRoutes } from './static';
 
 const router: Router = Router();
 
 appRoutes(router);
 staticRoutes(router);
+notFoundRoute(router);
+errorHandlerRoute(router);
 
 export default router;
