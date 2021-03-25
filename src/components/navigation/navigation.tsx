beb814df9871b4cf7cf9a175bd1805743d5128dd
Praktikumovsky <>
2021-03-25 23:20:17 +0300

    Фильтрация ссылок Navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,44 +1,39 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { cn } from '@bem-react/classname';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import textContent from './textContent';
+import { ApplicationState } from '../../index';
 import './navigation.css';
 
 const Cls = cn('navigation');
-export class Navigation extends PureComponent {
-  render() {
-    return (
+const LINKS = [
+  { to: '/authorization', text: textContent.auth, auhenticated: true },
+  { to: '/registration', text: textContent.reg, auhenticated: true },
+  { to: '/game', text: textContent.game },
+  { to: '/leaderboard', text: textContent.leaderbord },
+  { to: '/forum', text: textContent.forum, private: true },
+  { to: '/profile', text: textContent.profile, private: true },
+  { to: '/page404', text: textContent.page404 },
+  { to: '/page500', text: textContent.page500 },
+  { to: '/controls-demo', text: textContent.demo },
+];
+export default function Navigation():JSX.Element {
+  const isAuthenticated = useSelector((state:ApplicationState) => {
+    return state.auth.isAuthenticated;
+  });
+  const navigationLinks = LINKS.filter((x) => {
+    if (isAuthenticated) {
+      return !x.auhenticated;
+    }
+    return !x.private;
+  }).map((link) => { return <li key={link.to}><Link to={link.to}>{link.text}</Link></li>; });
+
+  return (
       <nav className={Cls()}>
         <ul>
-          <li>
-            <Link to="/authorization">{textContent.auth}</Link>
-          </li>
-          <li>
-            <Link to="/registration">{textContent.reg}</Link>
-          </li>
-          <li>
-            <Link to="/game">{textContent.game}</Link>
-          </li>
-          <li>
-            <Link to="/leaderboard">{textContent.leaderbord}</Link>
-          </li>
-          <li>
-            <Link to="/forum">{textContent.forum}</Link>
-          </li>
-          <li>
-            <Link to="/profile">{textContent.profile}</Link>
-          </li>
-          <li>
-            <Link to="/page404">{textContent.page404}</Link>
-          </li>
-          <li>
-            <Link to="/page500">{textContent.page500}</Link>
-          </li>
-          <li>
-            <Link to="/controls-demo">{textContent.demo}</Link>
-          </li>
+          {navigationLinks}
         </ul>
       </nav>
-    );
-  }
+  );
 }
