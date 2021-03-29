9be36a38e6082a606817a6aa5d13b30aebd096dd
Praktikumovsky <74737765+Praktikumovsky@users.noreply.github.com>
2021-03-29 19:36:27 +0300

    S7T81:  Сделать разбиение на роуты для залогина/незалогина (#37)
    * Серверная часть и начало клиента
    
    * Редиректы в клиентском роутере
    
    * Фильтрация ссылок Navigation
    
    * Правки по ревью
    
    Co-authored-by: Praktikumovsky <>

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Route } from 'react-router-dom';
 import './app.css';
-import { Navigation } from '../navigation/navigation';
+import Navigation from '../navigation/navigation';
 import { GameDescription } from '../gameDescription/gameDescription';
 import Authorization from '../../pages/authorization/authorization';
 import Registration from '../../pages/registration';
@@ -14,6 +14,8 @@ import ChangeData from '../../pages/changeData/changeData';
 import { Page404 } from '../../pages/page404/page404';
 import { Page500 } from '../../pages/page500/page500';
 import { ControlsDemo } from '../../pages/controlsDemo/controlsDemo';
+import PrivateRoute from '../privateRoute/privateRoute';
+import AuthenticatedRoute from '../authenticatedRoute/authenticatedRoute';
 
 export default class App extends PureComponent {
   render() {
@@ -23,14 +25,14 @@ export default class App extends PureComponent {
           <Navigation />
           <div>
                 <Route exact path="/" component={GameDescription}/>
-                <Route path="/authorization" component={Authorization}/>
-                <Route path="/registration" component={Registration}/>
+                <AuthenticatedRoute path="/authorization" component={Authorization} redirectTo="/"/>
+                <AuthenticatedRoute path="/registration" component={Registration} redirectTo="/"/>
                 <Route path="/game" component={Game}/>
                 <Route path="/leaderboard" component={Leaderboard}/>
-                <Route path="/forum" component={Forum}/>
-                <Route path="/profile" component={Profile}/>
-                <Route path="/change-password" component={ChangePassword}/>
-                <Route path="/change-data" component={ChangeData}/>
+                <PrivateRoute path="/forum" component={Forum} redirectTo="/authorization"/>
+                <PrivateRoute path="/profile" component={Profile} redirectTo="/authorization"/>
+                <PrivateRoute path="/change-password" component={ChangePassword } redirectTo="/authorization"/>
+                <PrivateRoute path="/change-data" component={ChangeData} redirectTo="/authorization"/>
                 <Route path="/page404" component={Page404}/>
                 <Route path="/page500" component={Page500}/>
                 <Route path="/controls-demo" component={ControlsDemo}/>
