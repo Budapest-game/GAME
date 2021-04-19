db8829bf50b9213ef7c27e795262bfc450fe5b4e
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-04-19 15:31:28 +0300

    Ревью фикс

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -2,4 +2,4 @@ import { connect } from 'react-redux';
 import { fetchThemeCSS } from '../../store/actions/theme';
 import Navigation from './navigation';
 
-export default connect(() => { return {}; }, { fetchCSS: fetchThemeCSS })(Navigation);
+export default connect(null, { fetchCSS: fetchThemeCSS })(Navigation);
