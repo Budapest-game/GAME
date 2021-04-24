a1a010c22a4932f521829893cdc41aaaad1b513c
Praktikumovsky <>
2021-04-24 15:18:00 +0300

    Используем LeaderboardAPI

--- src/components/leader/leader.tsx
+++ src/components/leader/leader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { cn } from '@bem-react/classname';
+import { BASE_URL } from '../../api/constants';
 import { LeaderInfo } from '../../store/actionCreators/leaderboard';
 import './leader.css';
 
@@ -9,7 +10,7 @@ export function Leader(info: LeaderInfo): JSX.Element {
   return <div className={Cls()} key={info.position}>
       <div className={Cls('position')}>{info.position}</div>
       <div className={Cls('media')}>
-        <img className={Cls('avatar')} src={info.avatar}/>
+        <img className={Cls('avatar')} src={BASE_URL + info.avatar}/>
       </div>
       <div className={Cls('nickname')}>
         <span>{info.name}</span>
